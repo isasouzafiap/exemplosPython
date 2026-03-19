fd4ccb84603ceeac6c2dc5944665200dca1fe79d
Isabelle <isasouzalima80@gmail.com>
2026-03-18 22:27:54 -0300

    aula de python, exemplos aula

---
+++ exemplo1.py
@@ -0,0 +1,9 @@
+ordem = input("Você quer execultar as instruções do bloco (s/n)?")
+
+if ordem == 's' :
+     print("Perfeito, vc esta dentro do bloco de instruções")
+     print("definido plwlo comando if")
+     print("outros comandos da linguagem também permite a criação de bloco")
+
+     print("primeira instrução fora do bloco if")
+     print("Fim do programa")
